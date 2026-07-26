82442f11740b0e7ecc238b63f02597f3e1125d50
antr-bell <antraviousbell123.com>
2026-07-26 16:14:16 -0400

    added egress rule

--- network.tf
+++ network.tf
@@ -68,5 +68,12 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   to_port           = 80
 }
 
+# 7. Egress Rule (All Outbound Allowed)
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.web_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
 # NOTE: If you temporarily change from_port = 22 and to_port = 22,
 # Sentinel will block your HCP Terraform plan automatically!
